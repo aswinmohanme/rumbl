fba41cd7dbbf7d3ce1d57099e5ba60c8de693719
aswinmohanme <dev.aswinmohan@gmail.com>
2019-11-17 21:47:35 +0530

    Implement registration

--- lib/rumbl/accounts/accounts.ex
+++ lib/rumbl/accounts/accounts.ex
@@ -20,4 +20,24 @@ defmodule Rumbl.Accounts do
   def get_user_by(params) do
     Repo.get_by(User, params)
   end
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def change_registration(%User{} = user, params) do
+    User.registration_changeset(user, params)
+  end
+
+  def register_user(attrs \\ %{}) do
+    %User{}
+    |> User.registration_changeset(attrs)
+    |> Repo.insert()
+  end
 end
