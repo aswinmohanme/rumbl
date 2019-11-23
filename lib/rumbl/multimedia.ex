a359abe96898f146d80d9f1b6467aa45c3f752ab
aswinmohanme <dev.aswinmohan@gmail.com>
2019-11-23 07:02:29 +0530

    Add category queries

--- lib/rumbl/multimedia.ex
+++ lib/rumbl/multimedia.ex
@@ -123,4 +123,10 @@ defmodule Rumbl.Multimedia do
   def create_category!(name) do
     Repo.insert!(%Category{name: name}, on_conflict: :nothing)
   end
+
+  def list_categories_alphabetically do
+    Category
+    |> Category.alphabetical()
+    |> Repo.all()
+  end
 end
